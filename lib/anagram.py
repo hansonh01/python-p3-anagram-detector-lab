4f321f5e4adca6b82d41cd207b0c198134fadd0e
Hanson <hanson.hua903@gmail.com>
2023-11-06 01:27:57 -0600

    Complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, candidates):
+        matches = []
+
+        sortWord = sorted(self.word.lower())
+
+        for candidate in candidates:
+            sortCandidate = sorted(candidate.lower())
+            if candidate.lower() != self.word.lower() and sortCandidate == sortWord:
+                matches.append(candidate)
+        return matches
